11c063b54696618ef9138e37f9779c3de3012e21
develop <develop@kaaiot.io>
2018-11-15 23:39:34 +0200

    ASG group with 3 worker nodes which launch using custom ami for docker swarm cluster

---
+++ README.md
@@ -0,0 +1,17 @@
+# Auto Scaling Group fow swarm worker
+
+Configuration in this directory creates Launch Configuration and Auto Scaling Group.
+
+Data sources are used to discover existing VPC resources (VPC, subnet and security group) as well as AMI details.
+
+## Usage
+
+To run this example you need to execute:
+
+```bash
+$ terraform init
+$ terraform plan
+$ terraform apply
+```
+
+Note that this example may create resources which cost money. Run `terraform destroy` when you don't need these resources.
---
+++ main.tf
@@ -0,0 +1,99 @@
+provider "aws" {
+  region = "us-east-1"
+
+  # Make it faster by skipping something
+  skip_get_ec2_platforms      = true
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+}
+
+##############################################################
+# Data sources to get VPC, subnets and security group details
+##############################################################
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "all" {
+  vpc_id = "${data.aws_vpc.default.id}"
+}
+
+data "aws_security_group" "default" {
+  vpc_id = "${data.aws_vpc.default.id}"
+  name   = "default"
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name = "name"
+
+    values = [
+      "docker-17.12.1-ce-ubuntu-worker",
+    ]
+  }
+}
+
+######
+# Launch configuration and autoscaling group
+######
+module "example" {
+  source = "../../"
+
+  name = "example-worker-ec2"
+
+  # Launch configuration
+  #
+  # launch_configuration = "my-existing-launch-configuration" # Use the existing launch configuration
+  # create_lc = false # disables creation of launch configuration
+  lc_name = "worker-lc"
+
+  image_id                     = "${data.aws_ami.amazon_linux.id}"
+  instance_type                = "t2.micro"
+  security_groups              = ["${data.aws_security_group.default.id}"]
+  associate_public_ip_address  = true
+  recreate_asg_when_lc_changes = true
+  key_name                     = "test_deploy"
+
+  ebs_block_device = [
+    {
+      device_name           = "/dev/xvdz"
+      volume_type           = "gp2"
+      volume_size           = "50"
+      delete_on_termination = true
+    },
+  ]
+
+  root_block_device = [
+    {
+      volume_size           = "50"
+      volume_type           = "gp2"
+      delete_on_termination = true
+    },
+  ]
+
+  # Auto scaling group
+  asg_name                  = "workar-sg"
+  vpc_zone_identifier       = ["${data.aws_subnet_ids.all.ids}"]
+  health_check_type         = "EC2"
+  min_size                  = 3
+  max_size                  = 3
+  desired_capacity          = 3
+  wait_for_capacity_timeout = 0
+
+  tags = [
+    {
+      key                 = "Environment"
+      value               = "dev"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Project"
+      value               = "megasecret"
+      propagate_at_launch = true
+    },
+  ]
+}
